9e7f5c3e3f7b9f8948d2cd214dd2588bba48f46f
vanshmehandru <vanshxzaddy@gmail.com>
2026-03-10 14:29:49 +0530

    UC5 - Booking Request

--- BookMyStay/src/BookMyStay.java
+++ BookMyStay/src/BookMyStay.java
@@ -1,65 +1,39 @@
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
-abstract class Room4 {
-    int beds;
-    int size;
-    double price;
+// Represents a guest's booking request
+class Reservation {
+    String guestName;
+    String roomType;
 
-    Room4(int beds, int size, double price) {
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
+    Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 
-    void display(String type, int available) {
-        System.out.println(type + " Room:");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price per night: " + price);
-        System.out.println("Available: " + available);
-        System.out.println();
-    }
-}
-
-class SingleRoom4 extends Room4 {
-    SingleRoom4() { super(1, 250, 1500.0); }
-}
-
-class DoubleRoom4 extends Room4 {
-    DoubleRoom4() { super(2, 400, 2500.0); }
-}
-
-class SuiteRoom4 extends Room4 {
-    SuiteRoom4() { super(3, 750, 5000.0); }
-}
-
-class RoomInventory4 {
-    HashMap<String, Integer> inventory = new HashMap<>();
-
-    RoomInventory4() {
-        inventory.put("Single", 5);
-        inventory.put("Double", 3);
-        inventory.put("Suite", 2);
-    }
-
-    int getAvailability(String type) {
-        return inventory.getOrDefault(type, 0);
+    void process() {
+        System.out.println("Processing booking for Guest: " + guestName + ", Room Type: " + roomType);
     }
 }
 
 public class BookMyStay {
+
     public static void main(String[] args) {
 
-        System.out.println("Room Search\n");
+        System.out.println("Booking Request Queue\n");
 
-        RoomInventory4 inventory = new RoomInventory4();
+        // Create a queue to hold booking requests
+        Queue<Reservation> bookingQueue = new LinkedList<>();
 
-        Room4 single = new SingleRoom4();
-        Room4 doubleRoom = new DoubleRoom4();
-        Room4 suite = new SuiteRoom4();
+        // Simulate guest booking requests
+        bookingQueue.add(new Reservation("Abhi", "Single"));
+        bookingQueue.add(new Reservation("Subha", "Double"));
+        bookingQueue.add(new Reservation("Vanmathi", "Suite"));
 
-        single.display("Single", inventory.getAvailability("Single"));
-        doubleRoom.display("Double", inventory.getAvailability("Double"));
-        suite.display("Suite", inventory.getAvailability("Suite"));
+        // Process requests in FIFO order
+        while (!bookingQueue.isEmpty()) {
+            Reservation request = bookingQueue.poll(); // removes from head
+            request.process();
+        }
     }
 }
